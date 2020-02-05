6f9384a1b0ab8bef475abec7ef8993d4a1b7ad60
Chris Paradeise <chris.paradeise@gmail.com>
2020-02-04 16:16:12 -0800

    Added correct homework starting point

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,9 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
-  "/favorites.html",
-  "/topic.html",
-  "/assets/css/style.css",
-  "/dist/app.bundle.js",
-  "/dist/favorites.bundle.js",
-  "/dist/topic.bundle.js"
+  "/db.js",
+  "/index.js",
+  "/style.css",
 ];
 
 
--- server.js
+++ server.js
@@ -15,13 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-// routes here
-require("./routes/htmlRoutes.js")(app);
+// routes
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
